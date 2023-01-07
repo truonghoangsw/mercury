efa44e0c42eb8142e31482e3292caed12a724741
dattt <dattt@creativeforce.io>
2023-01-08 01:20:10 +0700

    update config

--- Mercury.API/Mercury.API/Program.cs
+++ Mercury.API/Mercury.API/Program.cs
@@ -3,12 +3,20 @@ using Mercury.API.SignIrServices;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR().AddJsonProtocol(o =>
 {
     o.PayloadSerializerOptions.PropertyNamingPolicy = null;
     
 });
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseAuthorization();
 app.UseRouting();
