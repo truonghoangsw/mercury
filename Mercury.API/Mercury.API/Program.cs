5aa7d4eb1f1ab40942a68c003db7656436ffe5b2
truonghd <truonghd@creativeforce.io>
2023-01-08 00:55:28 +0700

    add routing

--- Mercury.API/Mercury.API/Program.cs
+++ Mercury.API/Mercury.API/Program.cs
@@ -25,11 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
-//app.MapControllers();
+app.UseRouting();
 app.MapHub<HubSockets>("/all", options =>
 {
     options.Transports = HttpTransportType.WebSockets;
