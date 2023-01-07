2ea0eab0de8f2f24165e820eb6d7efe365ee1903
truonghd <truonghd@creativeforce.io>
2023-01-07 21:41:42 +0700

    model Scores

---
+++ Mercury.API/Mercury.API/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace Mercury.API.Models
+{
+    public class Clans
+    {
+        public Guid ClanId { get; set; }
+        public List<Room> Rooms { get;set;}
+    }
+    public class Scores
+    {
+    }
+    public class Room
+    {
+        public Guid RoomId { get; set; }
+        public List<User> Users { get; set; }
+        public List<Scores> Scores { get; set; }
+    }
+    public class User
+    {
+        public string Name { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
---
+++ Mercury.API/Mercury.API/Services/RoomServices.cs
@@ -0,0 +1,7 @@
+﻿namespace Mercury.API.Services
+{
+    public class RoomServices
+    {
+        public int MyProperty { get; set; }
+    }
+}
