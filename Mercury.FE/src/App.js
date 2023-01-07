2c7200fdac38a250d7fad10b588962912eb2c4c4
Hoang Thai Hoc <hoc@creativeforce.io>
2023-01-08 00:55:19 +0700

    Add React router

--- Mercury.FE/src/App.js
+++ Mercury.FE/src/App.js
@@ -1,10 +1,13 @@
+import {Route, Routes} from 'react-router-dom';
+import Home from './pages/home/Home';
 import Game from './pages/game/Game';
 
 function App() {
   return (
-    <>
-      <Game/>
-    </>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/play" element={<Game/>}/>
+    </Routes>
   );
 }
 
