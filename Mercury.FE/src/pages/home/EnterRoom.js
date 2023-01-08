b83acac2104ad5025feb1daadfe0e2e7c1900020
Hoang Thai Hoc <hoc@creativeforce.io>
2023-01-08 14:23:38 +0700

    Home UI

--- Mercury.FE/src/pages/home/EnterRoom.js
+++ Mercury.FE/src/pages/home/EnterRoom.js
@@ -1,43 +1,43 @@
-import React, { useCallback, useState } from "react";
-import ws from "../../common/ws";
+import React, {useCallback, useState} from 'react';
+import ws from '../../common/ws';
 
-function EnterRoom({ userId, onBack }) {
-  const [roomId, setRoomId] = useState("");
-
-  const BackToHome = () => {
-    setRoomId("");
-    onBack();
-  };
+function EnterRoom({userId, setIsShowEnterRoom}) {
+  const [roomId, setRoomId] = useState('');
 
   const onRoomIdChane = useCallback((event) => {
     setRoomId(event.target.value);
   }, []);
 
-  const onSubmit = useCallback(
-    (event) => {
-      event.preventDefault();
-      if (!roomId) {
-        return;
-      }
-      ws.invoke("EnterRoom", {
-        userId,
-        roomId,
-      }).catch((error) => {
-        console.error(error);
-        alert("Error: " + error.message);
-      });
-    },
-    [userId, roomId]
-  );
+  const onSubmit = useCallback((event) => {
+    event.preventDefault();
+    if (!roomId) {
+      return;
+    }
+    ws.invoke('EnterRoom', {
+      userId,
+      roomId,
+    }).catch((error) => {
+      console.error(error);
+      alert('Error: ' + error.message);
+    });
+  }, [userId, roomId]);
+
+  const handleBack = useCallback(() => {
+    setIsShowEnterRoom(false);
+  }, []);
 
   return (
     <>
       <form action="" method="post" onSubmit={onSubmit}>
         <div className="input-group">
-          <input placeholder="RoomID" value={roomId} onChange={onRoomIdChane} />
-          <button onClick={BackToHome}>Back</button>
+          <input
+            placeholder="RoomID"
+            value={roomId}
+            onChange={onRoomIdChane}
+          />
         </div>
         <button type="submit">Join</button>
+        <button className="btn-default" type="button" onClick={handleBack}>Back</button>
       </form>
     </>
   );
